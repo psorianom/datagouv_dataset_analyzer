abc0e9dead718dc43006eb0ca03d69a9e5f615d7
Pavel Soriano <pavel.soriano@data.gouv.fr>
2019-04-10 11:25:56 +0200

    Added url and id cols to downloader. Added output folder to run csv detective

--- src/csv_downloader.py
+++ src/csv_downloader.py
@@ -1,22 +1,48 @@
+'''Downloads the URLs specified found in a csv (given the right column) and saves it with the resource id
+
+Usage:
+    csv_downloader.py <i> <o> <c> <r> [options]
+
+Arguments:
+    <i>             An input csv that contains a column with urls to download
+    <o>             The folder path where the files are gonna be downloaded
+    <c>             The column that contains the URLs
+    <r>             The column containing the resource id
+    --cores CORES   The number of cores to use in a parallel setting [default:2:int]
+'''
+
+
 import subprocess
+from asyncore import file_dispatcher
+
 import pandas as pds
+from argopt import argopt
 from joblib import Parallel, delayed
-from tqdm import tqdm
 
 
-def downloader(url, id):
+def downloader(url, id, output_folder):
 
-    p = subprocess.Popen(["wget", "-N", "/data/datagouv/csv/{}.csv".format(id[2:-2]), url])
+    p = subprocess.Popen(["wget", "-O", "{0}/{1}.csv".format(output_folder, idl), url])
     p.communicate()  # now wait plus that you can send commands to process
 
-def main():
-    df = pds.read_csv("all_datagouv_csv.csv")
 
-    urls = df.url.values[:]
-    ids = df._id.values[:]
+def get_files(file_path, output_folder, url_column, id_column, n_jobs):
+    df = pds.read_csv(file_path)
+    urls = df[url_column].values[:]
+    ids = df[id_column].values[:]
     assert(len(urls) == len(ids))
-    Parallel(n_jobs=18)(
-        delayed(downloader)(url,id) for url, id in zip(urls, ids))
+
+    Parallel(n_jobs=n_jobs)(
+        delayed(downloader)(url, id, output_folder) for url, id in zip(urls, ids))
 
 if __name__ == '__main__':
-    main()
+    # try_detective(begin_from="DM1_2018_EHPAD")
+    parser = argopt(__doc__).parse_args()
+    file_path = parser.i
+    output_folder = parser.o
+    url_column = parser.c
+    id_column = parser.r
+    n_jobs = int(parser.cores)
+
+    get_files(file_path, output_folder, url_column, id_column, n_jobs)
+
--- src/run_csv_detective.py
+++ src/run_csv_detective.py
@@ -4,9 +4,12 @@ Usage:
     run_csv_detective.py <i> [options]
 
 Arguments:
-    <i>                   An input file or directory (if dir it will convert all txt files inside).
+    <i>                                An input file or directory (if dir it will convert all txt files inside).
+    --output FOLDER                    Folder where to store the output structures [default:.]
     --cores=<n> CORES                  Number of cores to use [default: 2]
+    -s                                 Create structures only, after a csv_analysis
 '''
+import os
 from collections import defaultdict
 from argopt import argopt
 from csv_detective.explore_csv import routine
@@ -15,6 +18,7 @@ import pickle
 from functools import partial
 from tqdm import tqdm
 import logging
+import pandas as pd
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -32,13 +36,13 @@ def run_csv_detective(file_path):
 
     if len(inspection_results) > 2 and len(inspection_results["columns"]):
         inspection_results["file"] = file_path
-        print(file_path, inspection_results)
+        logger.info(file_path, inspection_results)
         return inspection_results
     else:
         logger.info("Analysis output of file {} was empty".format(files_path))
 
 
-def build_dataframes(list_dict_results):
+def build_structures(list_dict_results):
     """
     Builds two dataframes from a list of csv_detective results
     :param list_dict_results:
@@ -49,12 +53,17 @@ def build_dataframes(list_dict_results):
     # for res in list_dict_results:
     #     uniq_csv_detective_cols.update(res.keys())
     # print(uniq_csv_detective_cols)
+
+    type_dict_csv = build_type_dict(list_dict_results)
+
     list_dict = []
     for dicto in list_dict_results:
-        typed_columns = dicto.pop("columns")
+        dicto.pop("columns")
         list_dict.append(dicto)
 
-    pass
+    csv_detective_df = pd.DataFrame(list_dict)
+    print(csv_detective_df)
+    return type_dict_csv, csv_detective_df
 
 
 def get_csv_detective_analysis_single(files_path="./data/unpop_datasets", begin_from=None, n_datasets=None):
@@ -73,7 +82,7 @@ def get_csv_detective_analysis_single(files_path="./data/unpop_datasets", begin_
         list_dict_result.append(output_csv_detective)
 
     list_dict_result = [d for d in list_dict_result if d]
-    pickle.dump(list_dict_result, open("list_csv_detective_results.pkl", "wb"))
+    return list_dict_result
 
 
 def get_csv_detective_analysis(files_path="./data/unpop_datasets", begin_from=None, n_datasets=None, n_jobs=2):
@@ -90,54 +99,49 @@ def get_csv_detective_analysis(files_path="./data/unpop_datasets", begin_from=No
     list_dict_result = Parallel(n_jobs=n_jobs)(
         delayed(run_csv_detective_p)(file_path) for file_path in tqdm(list_files))
     list_dict_result = [d for d in list_dict_result if d]
-    pickle.dump(list_dict_result, open("list_csv_detective_results.pkl", "wb"))
+    return list_dict_result
 
 
-def try_detective(begin_from=None):
-    dict_column_dataset = {}
-    list_files = get_files("./data/unpop_datasets")
-    for file_path in list_files:
-        if begin_from and begin_from not in file_path:
-            continue
-
-        logger.info(file_path)
-        # with open(file_path, "r") as filo:
-        inspection_results = routine(file_path)
-        if len(inspection_results) > 2 and len(inspection_results["columns"]):
-            logger.info(file_path, inspection_results["columns"])
-            for k, v in inspection_results["columns"].items():
-                for v2 in v:
-                    dict_column_dataset[v2] = (file_path, k)
-            pickle.dump(dict_column_dataset, open("dict_col_dataset.pkl", "wb"))
-
-        else:
-            logger.info(file_path, inspection_results)
+def get_id(file_name):
+    return os.path.basename(file_name)[:-4]
 
 
-def analyze_detected_csvs():
-    list_csv_detective = pickle.load(open("list_csvdetective_results.pkl", "rb"))
-    list_csv_detective = [l for l in list_csv_detective if l]
+def build_type_dict(list_dict_results):
     dict_column_dataset = defaultdict(list)
-    for l in list_csv_detective:
+    for l in list_dict_results:
         for k, v in l["columns"].items():
             if "       " in k:
                 continue
+            l["file_id"] = get_id(l["file"])
             for v2 in v:
-                dict_column_dataset[v2].append((l["file"], k))
+                dict_column_dataset[v2].append((l["file_id"], k))
 
-    pickle.dump(dict_column_dataset, open("dict_column_dataset.pkl", "wb"))
+    return dict_column_dataset
 
 
 if __name__ == '__main__':
     # try_detective(begin_from="DM1_2018_EHPAD")
     parser = argopt(__doc__).parse_args()
     files_path = parser.i
+    output_path = parser.output
     n_cores = int(parser.cores)
+    structures_only = parser.s
 
-    if n_cores > 1:
-        get_csv_detective_analysis(files_path, begin_from=None, n_datasets=None, n_jobs=n_cores)
-    else:
-        get_csv_detective_analysis_single(files_path, begin_from=None, n_datasets=None)
+    if not structures_only:
+        if n_cores > 1:
+            list_dict_result = get_csv_detective_analysis(files_path, begin_from=None, n_datasets=None, n_jobs=n_cores)
+        else:
+            list_dict_result = get_csv_detective_analysis_single(files_path, begin_from=None, n_datasets=None)
 
-    build_dataframes(pickle.load(open("list_csv_detective_results.pkl", "rb")))
+        pickle.dump(list_dict_result, open(os.path.join(output_path, "list_csv_detective_results.pkl"), "wb"))
+
+    assert (os.path.isfile(os.path.join(output_path, "list_csv_detective_results.pkl")))
+
+    type_dict_csv, csv_detective_df = build_structures(pickle.load(
+        open(os.path.join(output_path, "list_csv_detective_results.pkl"), "rb")))
+
+    pickle.dump(type_dict_csv, open(os.path.join(output_path, "csv_detective_column_type.pkl"), "wb"))
+
+    csv_detective_df.to_csv(os.path.join(output_path, "csv_detective_attributes.csv"))
+
     pass
